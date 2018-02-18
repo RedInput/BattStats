2ece0885716cb9e8ebe1fb9a9af8d4be60d686a2
Ismael Reyes <ismael.kix2902@gmail.com>
2018-02-18 21:31:25 +0100

    Added receiver companion object to application subclass

--- app/src/main/java/com/redinput/batterytextwidget/BatteryWidgetApplication.kt
+++ app/src/main/java/com/redinput/batterytextwidget/BatteryWidgetApplication.kt
@@ -6,14 +6,20 @@ val prefs: PreferenceHelper by lazy {
     BatteryWidgetApplication.prefs!!
 }
 
+val receiver: BatteryReceiver by lazy {
+    BatteryWidgetApplication.receiver!!
+}
+
 class BatteryWidgetApplication : Application() {
 
     companion object {
         var prefs: PreferenceHelper? = null
+        var receiver: BatteryReceiver? = null
     }
 
     override fun onCreate() {
         prefs = PreferenceHelper(applicationContext)
+        receiver = BatteryReceiver()
         super.onCreate()
     }
 }
