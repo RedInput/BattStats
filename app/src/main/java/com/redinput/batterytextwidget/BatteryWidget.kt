e235c63c8c7b00e63a4b1672e0e81de38f096164
Ismael Reyes <ismael.kix2902@gmail.com>
2018-02-18 02:13:25 +0100

    Updated widget provider with basic functionality to test battery status receiving

--- app/src/main/java/com/redinput/batterytextwidget/BatteryWidget.kt
+++ app/src/main/java/com/redinput/batterytextwidget/BatteryWidget.kt
@@ -2,41 +2,66 @@ package com.redinput.batterytextwidget
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.BatteryManager
+import android.util.Log
 import android.widget.RemoteViews
 
 class BatteryWidget : AppWidgetProvider() {
 
-    public override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        val action = intent.action
+        if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val thisWidget = ComponentName(context, BatteryWidget::class.java)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
+            if (appWidgetIds.isNotEmpty()) {
+                onUpdate(context, appWidgetManager, appWidgetIds)
+            }
         }
     }
 
-    public override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        val batteryStatus = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, batteryStatus)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
         for (appWidgetId in appWidgetIds) {
 
         }
     }
 
-    public override fun onEnabled(context: Context) {
+    override fun onEnabled(context: Context) {
         val batteryReceiver = BatteryWidget()
         val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        context.registerReceiver(batteryReceiver, intentFilter)
+        Log.d("asdasdasd",batteryReceiver.toString())
+        context.applicationContext.registerReceiver(batteryReceiver, intentFilter)
 
     }
 
-    public override fun onDisabled(context: Context) {
+    override fun onDisabled(context: Context) {
         val batteryReceiver = BatteryWidget()
-        context.unregisterReceiver(batteryReceiver)
+        Log.d("asdasdasd",batteryReceiver.toString())
+        context.applicationContext.unregisterReceiver(batteryReceiver)
     }
 
     companion object {
 
-        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int, batteryStatus: Intent) {
             val views = RemoteViews(context.getPackageName(), R.layout.battery_widget)
+
+            val level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val selection = prefs.getWidgetStyle(appWidgetId)
+
+            views.setTextViewText(R.id.appwidget_text, level.toString() + " - " + selection.name)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
