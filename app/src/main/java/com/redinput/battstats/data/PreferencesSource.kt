33e2e186517d990e6b926e47afbd8dd16221085d
Ismael Reyes <ismael.kix2902@gmail.com>
2020-09-20 21:05:48 +0200

    Created preferencesSource interface

---
+++ app/src/main/java/com/redinput/battstats/data/PreferencesSource.kt
@@ -0,0 +1,9 @@
+package com.redinput.battstats.data
+
+import com.redinput.battstats.Widget
+
+interface PreferencesSource {
+    fun saveWidgetInfo(info: Widget.Config)
+    fun loadWidgetInfo(id: Int): Widget.Config
+    fun removeWidgetInfo(id: Int)
+}
