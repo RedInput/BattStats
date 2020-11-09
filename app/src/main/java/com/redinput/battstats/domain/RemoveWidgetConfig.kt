3b9ce33cb0a1f448d5968ebe8837fcb0d763e2ed
Ismael Reyes <ismael.kix2902@gmail.com>
2020-11-09 18:35:17 +0100

    Removed unused import

--- app/src/main/java/com/redinput/battstats/domain/RemoveWidgetConfig.kt
+++ app/src/main/java/com/redinput/battstats/domain/RemoveWidgetConfig.kt
@@ -2,7 +2,6 @@ package com.redinput.battstats.domain
 
 import com.redinput.battstats.data.PreferencesRepository
 import com.redinput.battstats.helpers.UseCase
-import kotlinx.coroutines.CoroutineScope
 
 class RemoveWidgetConfig(private val repository: PreferencesRepository) : UseCase<UseCase.None, Int>() {
     override suspend fun run(params: Int): Result {
--- app/src/main/java/com/redinput/battstats/domain/SaveWidgetConfig.kt
+++ app/src/main/java/com/redinput/battstats/domain/SaveWidgetConfig.kt
@@ -3,7 +3,6 @@ package com.redinput.battstats.domain
 import com.redinput.battstats.Widget
 import com.redinput.battstats.data.PreferencesRepository
 import com.redinput.battstats.helpers.UseCase
-import kotlinx.coroutines.CoroutineScope
 
 class SaveWidgetConfig(private val repository: PreferencesRepository) : UseCase<UseCase.None, Widget.Config>() {
     override suspend fun run(params: Widget.Config): Result {
